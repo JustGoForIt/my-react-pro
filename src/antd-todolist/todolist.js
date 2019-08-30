2fc7cd8a91614a9a244026bdffd947bf64fd482a
luoyi3 <luoyi3@beisen.com>
2019-08-30 23:27:00 +0800

    actionTypes 和 actionCreators

--- src/antd-todolist/todolist.js
+++ src/antd-todolist/todolist.js
@@ -2,28 +2,56 @@ import React, { Component } from 'react'
 
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
-
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './../store/actionCreators'
 import store from './../store'
 
 class Todolist extends Component {
   constructor (props) {
     super(props)
+    debugger
     this.state = store.getState()
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    this.handleBtnCilck = this.handleBtnCilck.bind(this)
+    this.handleItemDelete = this.handleItemDelete.bind(this)
+    store.subscribe(this.handleStoreChange)
   }
 
   render () {
     return (<div style={{ 'marginLeft': '10px', 'marginTop': '10px' }} >
-      <Input placeholder='todo info' style={{ 'width': '300px', 'marginRight': '10px' }} />
-      <Button type='primary'>提交</Button>
+      <Input
+        value={this.state.inputValue}
+        placeholder='todo info'
+        style={{ 'width': '300px', 'marginRight': '10px' }}
+        onChange={this.handleInputChange} />
+      <Button type='primary' onClick={this.handleBtnCilck}>提交</Button>
 
       <List
         style={{ 'marginTop': '30px', 'width': '300px' }}
         size='small'
         bordered
         dataSource={this.state.list}
-        renderItem={item => <List.Item>{item}</List.Item>}
+        renderItem={(item, index) => <List.Item onClick={(e) => { this.handleItemDelete(index, e) }}>{item}</List.Item>}
       />
     	</div>)
+  }
+  handleInputChange (e) {
+  	const action = getInputChangeAction(e.target.value)
+  	store.dispatch(action)
+  }
+
+  handleStoreChange () {
+  	this.setState(store.getState())
+  }
+
+  handleBtnCilck () {
+  	const action = getAddItemAction()
+  	store.dispatch(action)
+  }
+
+  handleItemDelete (index) {
+    const action = getDeleteItemAction(index)
+    store.dispatch(action)
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,28 @@
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from './actionTypes'
 const defaultState = {
   inputValue: '',
   list: [1, 3]
 }
 
+//
 export default (state = defaultState, action) => {
+  debugger
+  if (action.type === CHANGE_INPUT_VALUE) {
+  	// reducer可以接受state，但是绝对不能修改state
+  	const newState = JSON.parse(JSON.stringify(state))
+  	newState.inputValue = action.value
+  	return newState
+  }
+  if (action.type === ADD_TODO_ITEM) {
+  	const newState = JSON.parse(JSON.stringify(state))
+  	newState.list.push(newState.inputValue)
+  	newState.inputValue = ''
+  	return newState
+  }
+  if (action.type === DELETE_TODO_ITEM) {
+  	const newState = JSON.parse(JSON.stringify(state))
+  	newState.list.splice(action.index, 1)
+  	return newState
+  }
   return state
 }
